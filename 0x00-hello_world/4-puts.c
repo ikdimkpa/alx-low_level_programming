f1b04781a4961e0f54fd8cc8ed0ceec992c3b2f1
ikdimkpa <ikennadimkpa@gmail.com>
2022-12-09 06:01:27 -0800

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,13 @@
 #include <stdio.h>
 
 /**
+ *A function that prints Programmimg is like building a multilingual puzzle
  *The beginning of my C-Programming journey.
  *Please feel free to join me anywhere...
  */
 
 int main(void)
 {
-  puts ("\"Programming is like building a multilingual puzzle");
-  return (0);
+  puts("\"Programming is like building a multilingual puzzle");
+  return(0);
 }
