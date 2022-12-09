01dff17c26a1d9951cbbb7dcfaded684f650ecad
ikdimkpa <ikennadimkpa@gmail.com>
2022-12-09 05:56:27 -0800

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,10 @@
 #include <stdio.h>
 
+/**
+ *The beginning of my C-Programming journey.
+ *Please feel free to join me anywhere...
+ */
+
 int main(void)
 {
   puts ("\"Programming is like building a multilingual puzzle");
