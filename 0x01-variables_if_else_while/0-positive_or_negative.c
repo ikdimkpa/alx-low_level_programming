df2429ad52f3a9d64a3f68111f8ae31285af1740
ikdimkpa <ikennadimkpa@gmail.com>
2022-12-10 08:58:44 -0800

    0-positive_or_negative.c

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -17,11 +17,11 @@ n = rand() - RAND_MAX / 2;
  else
    if (n == 0)
      {
-       printf("%d is zero", n);
+       printf("%d is zero\n", n);
      }
        else
 	 {
-	   printf("%d is negative", n);
+	   printf("%d is negative\n", n);
 	 }
 return (0);
 }
