c62fcff2c39a45499b86a440ad5c00763de4ea27
ikdimkpa <ikennadimkpa@gmail.com>
2022-12-10 04:55:41 -0800

    6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -8,6 +8,6 @@
 
 int main(void)
 {
-printf("Size of a char: 1 byte(s)\n";"Size of an int: 4 byte(s)\n";"Size of a long int: 8 byte(s)\n";"Size of a long long int: 8 byte(s)\n";"Size of a float: 4 byte(s)\n");
+printf("Size of a char: 1 byte(s)\n""Size of an int: 4 byte(s)\n""Size of a long int: 8 byte(s)\n""Size of a long long int: 8 byte(s)\n""Size of a float: 4 byte(s)\n");
 return (0);
 }
