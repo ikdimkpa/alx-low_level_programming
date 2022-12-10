b81a6771c8dcb33a3d308cc84bb50bf495c6b4af
ikdimkpa <ikennadimkpa@gmail.com>
2022-12-10 06:50:11 -0800

    6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,17 +1,21 @@
 #include <stdio.h>
-#include <stdlib.h>
-
 /**
- * Main - A function that prints five lines of code.
- * Return - Always returns 0 (Successful)
+ * main - prints out sizes of data types in c.
+ * code by Dimkpa_the_Great!
+ * Return: 0
  */
 
 int main(void)
 {
-printf("Size of a char: 1 byte(s)\n");
-printf("Size of an int: 4 byte(s)\n");
-printf("Size of a long int: 8 byte(s)\n");
-printf("Size of a long long int: 8 byte(s)\n");
-printf("Size of a float: 4 byte(s)\n");
+char i;
+int ii;
+long int iii;
+long long int iv;
+float v;
+printf("Size of a char: %lu byte(s)\n", (unsigned long)sizeof(i));
+printf("Size of an int: %lu byte(s)\n", (unsigned long)sizeof(ii));
+printf("Size of a long int: %lu byte(s)\n", (unsigned long)sizeof(iii));
+printf("Size of a long long int: %lu byte(s)\n", (unsigned long)sizeof(iv));
+printf("Size of a float: %lu byte(s)\n", (unsigned long)sizeof(v));
 return (0);
 }
