8ef0bf30c5520b7a7dd4abf6031d2f745f3cb86e
ikdimkpa <ikennadimkpa@gmail.com>
2022-12-10 05:08:58 -0800

    6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -2,12 +2,16 @@
 #include <stdlib.h>
 
 /**
- * Main - Prints the size of various types on the computer it is compiled and run on.
+ * Main - Prints five lines of code.
  * Return 0 (Successful)
  */
 
 int main(void)
 {
-printf("Size of a char: 1 byte(s)\n""Size of an int: 4 byte(s)\n""Size of a long int: 8 byte(s)\n""Size of a long long int: 8 byte(s)\n""Size of a float: 4 byte(s)\n");
+printf("Size of a char: 1 byte(s)\n");
+printf("Size of an int: 4 byte(s)\n");
+printf("Size of a long int: 8 byte(s)\n");
+printf("Size of a long long int: 8 byte(s)\n");
+printf("Size of a float: 4 byte(s)\n");
 return (0);
 }
