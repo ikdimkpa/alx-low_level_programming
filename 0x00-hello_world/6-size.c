d77adc7463775edc90bf2c13406ddd9ccce77a64
ikdimkpa <ikennadimkpa@gmail.com>
2022-12-10 04:52:00 -0800

    6-size.c

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * Main - Prints the size of various types on the computer it is compiled and run on.
+ * Return 0 (Successful)
+ */
+
+int main(void)
+{
+printf("Size of a char: 1 byte(s)\n";"Size of an int: 4 byte(s)\n";"Size of a long int: 8 byte(s)\n";"Size of a long long int: 8 byte(s)\n";"Size of a float: 4 byte(s)\n");
+return (0);
+}
