e6e572bcb6cf52b09e51923c64263f34fc96e16c
ikdimkpa <ikennadimkpa@gmail.com>
2022-12-12 02:36:47 -0800

    2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * mainc-Entry point
+ * Return: Always 0
+ */
+int main(void)
+{
+	char c;
+	char a;
+	char z;
+
+	for (c = a; c < z; c++)
+		putchar(c);
+	putchar(*"\n");
+	return (0);
+}
