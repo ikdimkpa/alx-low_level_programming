fd674a1fc01b8414b277cb51c4fa1bf51e538fa8
ikdimkpa <ikennadimkpa@gmail.com>
2022-12-12 03:51:03 -0800

    3-print_alphabets.c

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main: Entry point
+ * Return: Always returns 0
+ */
+int main(void)
+{
+	char c;
+
+	for (c = 'a'; c <= 'z'; c++)
+		putchar(c);
+	for (c = 'A'; c <= 'Z'; c++)
+		putchar(c);
+	return (0);
+}
