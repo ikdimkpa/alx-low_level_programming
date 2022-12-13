3a147afdb0c3eb982292354dda17c46a2bd85c4d
ikdimkpa <ikennadimkpa@gmail.com>
2022-12-13 07:48:34 -0800

    0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,11 +1,22 @@
-#include <stdio.h>
+#include "main.h"
+#include <unistd.h>
+
 /**
- * main - Entry point
- * Return: 0
+ * main - Prints putchar followed by a new line
+ * Return: 0 (Success)
  */
+
 int main(void)
 {
-	write('_putchar');
-	putchar('\n');
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+
 	return (0);
 }
