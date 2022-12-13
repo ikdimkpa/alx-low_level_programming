c22f2e81cd785809185ba8ddfe7471be108988c4
ikdimkpa <ikennadimkpa@gmail.com>
2022-12-13 08:00:17 -0800

    1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * main - Prints all alphabets in lower case
+ * Return: 0(Success)
+ */
+int main(void)
+{
+	char c;
+
+	for (c = 'a'; c <='z'; c++)
+	{
+		_putchar(c);
+	}
+	return (0);
+}
