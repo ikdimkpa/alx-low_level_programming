1adda3424654b29ffb599c986d1dd67df1d290df
ikdimkpa <ikennadimkpa@gmail.com>
2022-12-15 04:52:49 -0800

    103-fibonacci.c

--- 0x02-functions_nested_loops/103-fibonacci.c
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -10,15 +10,13 @@ int main(void)
 	long j = 1, k = 2, sum = k;
 
 	while (k + j < 4000000)
-	{
+	{ 
 		k += j;
 		if (k % 2 == 0)
-		{
 			sum += k;
-			j = k - j;
-			++i;
-		}
+		j = k - j;
+		++i;
 	}
-	printf("%lu\n", sum);
+	printf("%ld\n", sum);
 	return (0);
 }
