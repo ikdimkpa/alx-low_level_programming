0e8f1e08e8c8b44952a8a2e72879aa3f5758c87e
ikdimkpa <ikennadimkpa@gmail.com>
2022-12-16 02:14:24 -0800

    3-print_numbers.c

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -2,7 +2,6 @@
 
 /**
  * print_numbers - Print the numbers since 0 up to 9
- * Return: The numbers since 0 up to 9
  */
 
 void print_numbers(void)
@@ -10,11 +9,10 @@ void print_numbers(void)
 	int x;
 
 	x = 0;
-
 	while (x <= 9)
 	{
 		_putchar(x + '0');
 		x++;
 	}
-	_putchar("\n");
+	_putchar('\n');
 }
