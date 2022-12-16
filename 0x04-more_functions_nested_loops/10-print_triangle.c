0f4850db13109d245600648994171b9ec1673f0b
ikdimkpa <ikennadimkpa@gmail.com>
2022-12-16 13:56:57 -0800

    10-print_triangle.c

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -6,21 +6,16 @@
  */
 void print_triangle(int size)
 {
-	int x, y, z;
+	int i, j;
 
-	if (size <= 0)
-		_putchar('\n');
-	else
+	for (i = 0; i < size; i++)
 	{
-		for (x = 0; x < size; x++)
-			for (y = size - x; y > 1; y--)
-			{
-				_putchar(32);
-			}
-		for (z = 0; z <= x; z++)
-		{
+		for (j = 1; j < (size - i); j++)
+			_putchar(' ');
+		for (j--; j < size; j++)
 			_putchar(35);
-		}
-		_putchar('\n');
+		if (i < (size - 1))
+			_putchar('\n');
 	}
+	_putchar('\n');
 }
