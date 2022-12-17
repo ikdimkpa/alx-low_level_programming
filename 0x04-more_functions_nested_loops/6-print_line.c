37cbda255a75599b86313b8f883d40f4bdf4c53e
ikdimkpa <ikennadimkpa@gmail.com>
2022-12-17 00:59:27 -0800

    6-print_line.c

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -2,8 +2,15 @@
 
 /**
  * print_line - Draw a straight line in the terminal
- * Return: 0 Always
+ * @n: times straight line is printed
  */
 void print_line(int n)
 {
+	int i;
 
+	for (i = 0; i < n; i++)
+	{
+		_putchar(95);
+	}
+	_putchar('\n');
+}
