0202000c69bce5d5d844382847c7e3e1b46a5e10
ikdimkpa <ikennadimkpa@gmail.com>
2022-12-19 15:55:25 -0800

    100-atoi.c

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -15,18 +15,18 @@ int _atoi(char *s)
 	temp = s;
 	num = 0;
 	sign = 1;
-	while (*temp != '\0' && (*temp < '0' || temp > '9'))
+	while (*temp != '\0' && (*temp < '0' || *temp > '9'))
 	{
-		if (temp == '-')
+		if (*temp == '-')
 			sign *= -1;
 		temp++;
 	}
-	if (temp != '\0')
+	if (*temp != '\0')
 	{
 		do {
 			num = num * 10 + (*temp - '0');
 			temp++;
-		} while (*temp >= '0' && *temp <= '9')
+		} while (*temp >= '0' && *temp <= '9');
 	}
 	return (num * sign);
 }
