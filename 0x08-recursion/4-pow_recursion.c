d8b307cacc72b7a12edbe92a3a1d2e39e3339d52
ikdimkpa <ikennadimkpa@gmail.com>
2023-01-05 09:24:09 -0800

    4-pow_recursion.c

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -11,6 +11,10 @@ int _pow_recursion(int x, int y)
 {
 	if (y < 0)
 		return (-1);
+	else if (y == 1)
+		return (x);
 	else if (y == 0)
 		return (1);
+
+	return (x * _pow_recursion(x + (y - 1)));
 }
