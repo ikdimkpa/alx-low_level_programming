160cb52bd54065952bb1cf25353ef9a90b22c36d
ikdimkpa <ikennadimkpa@gmail.com>
2023-01-05 15:47:20 -0800

    5-sqrt_recursion.c

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * help - checks if square root of number exists
+ * @num: number
+ * @sqrt: possible square root
+ *
+ * Return: square root of number or -1 for error
+ */
+int help(int num, int sqtr)
+{
+	if (sqrt * sqrt == num)
+		return (sqrt);
+	else if (sqrt * sqrt > num)
+		return (-1);
+	else
+		return (help(num, sqrt + 1));
+}
+
+/**
+ * _sqrt_recursion - returns the natural square root of a number.
+ * @n: number to be square-rooted
+ *
+ * Return: square errt of n, or -1 on failure
+ */
+int _sqrt_recursion(int n)
+{
+	if (n < 0)
+		return (-1);
+	return (help(n, 0));
+}
