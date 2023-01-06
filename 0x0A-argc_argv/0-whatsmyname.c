ba3854cf08b8917c51078114ef498db2d53e41b6
ikdimkpa <ikennadimkpa@gmail.com>
2023-01-06 11:10:59 -0800

    0-whatsmyname.c

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - function that prints its name
+ * @argc: argc parameter
+ * @argv: an array of a command listed
+ *
+ * Return: 0 on success
+ */
+int main(int argc, char *argv[])
+{
+	printf("%s\n", *argv);
+	return (0);
+}
