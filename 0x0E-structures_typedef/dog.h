7f133c8f42e41aa4b8c7ca6483a4da4435eaea50
ikdimkpa <ikennadimkpa@gmail.com>
2023-01-16 05:20:50 -0800

    dog.h

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,6 +1,12 @@
 #ifndef DOG_H
 #define DOG_H
 
+/**
+ * struct dog - structure containing members of types char and float.
+ * @name: first member of the structure.
+ * @age: second member of the structure.
+ * @owner: third member of the structure.
+ */
 struct dog
 {
 	char *name;
