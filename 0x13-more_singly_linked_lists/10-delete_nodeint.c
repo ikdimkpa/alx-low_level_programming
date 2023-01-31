21737545c47a8e1180a14f54ef13aa4364707a7d
ikdimkpa <ikennadimkpa@gmail.com>
2023-01-31 06:52:35 -0800

    10-delete_nodeint.c

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -20,6 +20,7 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 	{
 		*head = (*head)->next;
 		free(copy);
+		return (1);
 	}
 
 	for (node = 0; node < (index - 1); node++)
@@ -33,5 +34,5 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 	copy->next = temp->next;
 	free(temp);
 
-	return (-1);
+	return (1);
 }
