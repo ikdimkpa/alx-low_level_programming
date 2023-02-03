ac7051f40d26539245c5aa8a340dc2380b3edc2e
ikdimkpa <ikennadimkpa@gmail.com>
2023-02-03 11:34:15 -0800

    0-binary_to_uint.c

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -9,17 +9,30 @@
  */
 unsigned int binary_to_uint(const char *b)
 {
-	int n;
-	unsigned int dec_val = 0;
+	unsigned int dec_val;
+	int len, base_two;
 
 	if (!b)
 		return (0);
-	for (n = 0; b[n]; n++)
+
+	dec_val = 0;
+
+	for (len = 0; b[len] != '\0'; len++)
+		;
+
+	for (len--, base_two = 1; len >= 0; len--, base_two *= 2)
 	{
-		if (b[n] < 0 || b[n] > 1)
+		if (b[len] != '0' && b[len] != '1')
+		{
 			return (0);
-		dec_val = 2 * dec_val + (b[n] - '0');
+		}
+
+		if (b[len] & 1)
+		{
+			dec_val += base_two;
+		}
 	}
-	return (dec_val);
+
+return (dec_val);
 }
 
