5c287d2585685a62cc9e5f87c42bfd468b1f038e
Ikenna Dimkpa <ikennadimkpa@gmail.con>
2023-07-15 17:41:05 -0700

    0x1E-search_algorithms mandatory tasks completed!

--- 0x1E-search_algorithms/1-binary.c
+++ 0x1E-search_algorithms/1-binary.c
@@ -29,7 +29,7 @@ int binary_search(int *array, size_t size, int value)
 			printf("%d\n", array[index]);
 
 		if (array[mid] > value)
-			right = mid -1;
+			right = mid - 1;
 		else if (array[mid] < value)
 			left = mid + 1;
 		else
