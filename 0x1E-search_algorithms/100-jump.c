a9b7a7ef26a761dcba67348c876f0d638098f902
Ikenna Dimkpa <ikennadimkpa@gmail.com>
2023-09-16 23:17:27 +0000

    debugged 100-jump.c

--- 0x1E-search_algorithms/100-jump.c
+++ 0x1E-search_algorithms/100-jump.c
@@ -1,26 +1,26 @@
 #include "search_algos.h"
 
 /**
- *  * min - finds the minimum between two numbers
- *   * @a: first number
- *    * @b: second number
- *     *
- *      * Return: the minimum number
- *       */
+ * min - finds the minimum between two numbers
+ * @a: first number
+ * @b: second number
+ *
+ * Return: the minimum number
+ */
 size_t min(size_t a, size_t b)
 {
 	return ((a < b) ? a : b);
 }
 
 /**
- *  * jump_search - a function that searches for a value in a sorted array
- *   *		of integers using the Jump search algorithm
- *    * @array: is a pointer to the first element of the array to search in
- *     * @size: is the number of elements in array
- *      * @value: is the value to search for
- *       *
- *        * Return: the first index where value is located
- *         */
+ * jump_search - a function that searches for a value in a sorted array
+ * of integers using the Jump search algorithm
+ * @array: is a pointer to the first element of the array to search in
+ * @size: is the number of elements in array
+ * @value: is the value to search for
+ *
+ * Return: the first index where value is located
+ */
 int jump_search(int *array, size_t size, int value)
 {
 	size_t prev = 0;
